22b29a2433371a9399882ac35cc5bb2646508727
themmixproject <steven.devalk@gmail.com>
2022-09-12 23:10:05 +0200

    Prevented scrolling when element is being moved

--- src/components/PullDelete.vue
+++ src/components/PullDelete.vue
@@ -6,6 +6,7 @@
             class="pull-item"
             @touchstart="touchStart($event, index)"
             @touchmove="touchMove($event, index)"
+            @touchend="touchEnd($event, index)"
         >
             Pull to delete item
             <div class="pull-button"></div>
@@ -17,25 +18,58 @@
 export default {
     data() {
         return {
-            items: [0, 0, 0],
+            items: [0, 0, 0, 0, 0],
             posX: 0,
             divX: 0,
             diffX: 0,
-            distanceMoved: 0
+            startPosX: 0,
+            posY: 0,
+            divY: 0,
+            diffY: 0,
+            startPosY: 0,
+            distanceMoved: 0,
+            elIsMoving: false,
+            isScrolling: false,
+            scrollingIsActivated: false
         };
     },
     methods: {
         touchStart(event) {
             this.posX = event.targetTouches[0].clientX;
+            // this.startPosX = event.targetTouches[0].clientX
+            this.startPosX = this.posX;
+            this.startPosY = event.targetTouches[0].pageY;
             this.divX = event.target.style.left.replace("px", "");
             this.diffX = this.posX - this.divX;
         },
         touchMove(event) {
+            if (this.elIsMoving) {
+                event.preventDefault();
+            }
+
             this.posX = event.targetTouches[0].clientX;
             let aX = this.posX - this.diffX;
 
-            let target = event.target;
-            target.style.left = aX + "px";
+            let posY = event.targetTouches[0].pageY;
+
+            if (Math.abs(this.posX - this.startPosX) > 3 && !this.isScrolling) {
+                event.preventDefault();
+                this.elIsMoving = true;
+                let target = event.target;
+                target.style.left = aX + "px";
+            } else if (
+                Math.abs(posY - this.startPosY) > 3 &&
+                !this.scrollingIsActivated &&
+                !this.elIsMoving
+            ) {
+                this.isScrolling = true;
+                this.scrollingIsActivated = true;
+            }
+        },
+        touchEnd() {
+            this.elIsMoving = false;
+            this.isScrolling = false;
+            this.scrollingIsActivated = false;
         }
     }
 };
@@ -58,5 +92,6 @@ export default {
     color: #4c4c4c;
     box-sizing: border-box;
     position: relative;
+    // touch-action: none;
 }
 </style>
