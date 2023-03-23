176d7a3a1d3dee2e81fd267cf335e567a40b9242
themmixproject <steven.devalk@gmail.com>
2023-03-23 14:25:13 +0100

    Added base for TerminalWindow component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-    <pull-delete />
+    <terminal-window />
 </template>
 
 <script>
 // import Test from "./components/TestComponent.vue";
-import PullDelete from "./components/PullDelete.vue";
+// import PullDelete from "./components/PullDelete.vue";
+import TerminalWindow from "./components/TerminalWindow.vue";
 
 export default {
     name: "App",
     components: {
-        PullDelete
+        TerminalWindow
     }
 };
 </script>
@@ -17,5 +18,19 @@ export default {
 <style lang="scss">
 body {
     margin: 0;
+    padding: 0;
+    background-color: black;
+    color: white;
+    font-family: "Courier New", Courier, monospace;
+}
+
+input {
+    border: 0;
+    font-family: "Courier New", Courier, monospace;
+    font-size: inherit;
+    padding: 0;
+    outline: 0;
+    // background-color: black;
+    color: white;
 }
 </style>
---
+++ src/components/TerminalWindow.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="terminal-container">
+        $~:> Hello World <input class="terminal-input" type="text" />
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
